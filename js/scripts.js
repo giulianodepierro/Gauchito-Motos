0c9273b788ccc27a562e5f7f1d63330f153652c3
giulianodepierro <53575513+giulianodepierro@users.noreply.github.com>
2021-05-25 17:59:15 -0300

    Armando submenu2

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 $(function() {
 
     'use strict';
+    var unNumero = 1;
+    console.log(unNumero);
 
     console.log("javascript OK");
     $('.rodados').click(function() {
+
+
         $('.flotante').css({ 'display': 'block' });
     });
 
